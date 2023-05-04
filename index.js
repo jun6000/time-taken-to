dd0786eb5a0745a96ae823b21dbfed5d76a97365
Arjun Vishanth <arjunvishanth@gmail.com>
2023-05-04 13:55:31 +0530

    Added options support.
    Used commander to implement options support.
    Users can now specify number of significant decimal digits to keep when rounding off (default is 6).
    Fixed cmd undefined error message.

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
 
 import { performance } from "perf_hooks";
 import { exec } from "child_process";
+import { program } from "commander";
 
-const cmd = process.argv.slice(2).join(' ');
+// Get command-line options and arguments
+
+program
+    .option("-r, --round-to <count>", "Round the result to <count> digits.");
+
+program.parse(process.argv);
+
+// Handle options
+
+const opts = program.opts();
+
+if (opts.roundTo) {
+    const roundTo = opts.roundTo;
+
+    if (/[^0-9]/.test(roundTo) || parseFloat(roundTo) > 15)
+        program.error(`error: option '-r, --round-to <count>' argument invalid (whole numbers <=15 only)`);
+}
+
+// Handle command to test
+
+const cmd = program.args.join(' ');
 
 if (!cmd) {
-    console.error(`Specify a command after "time-taken" to measure its execution time.`);
+    console.error(`error: Specify a command as command-line argument to measure its execution time`);
     process.exit(0);
 }
+
+// Measure execution time
 
 const startTime = performance.now();
 
@@ -25,7 +48,7 @@ exec(cmd, (err, stdout, stderr) => {
     const execTime = stopTime - startTime;      // Execution time measured in milliseconds
 
     if (execTime > 999)
-        console.log(`Time taken: ${ parseFloat((execTime / 1000).toFixed(6)) } s`);
+        console.log(`Time taken: ${ parseFloat((execTime / 1000).toFixed(opts.roundTo)) } s`);
     else
-        console.log(`Time taken: ${ parseFloat(execTime.toFixed(6)) } ms`);
+        console.log(`Time taken: ${ parseFloat(execTime.toFixed(opts.roundTo)) } ms`);
 });
